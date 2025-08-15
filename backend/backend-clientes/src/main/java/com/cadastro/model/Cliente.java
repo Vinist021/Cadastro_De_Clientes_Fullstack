2f089638326b9309b3e0bb4297b50970f71c101f
Vinicius Teixeira <vinist.002@gmail.com>
2025-08-15 14:38:32 -0300

    Criadas entidades 'Cliente' e 'Endereco' no backend

---
+++ backend/backend-clientes/src/main/java/com/cadastro/model/Cliente.java
@@ -0,0 +1,34 @@
+package com.cadastro.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "clientes")
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 150)
+    private String sobrenome;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id", referencedColumnName = "id")
+    private Endereco endereco;
+
+}
---
+++ backend/backend-clientes/src/main/java/com/cadastro/model/Endereco.java
@@ -0,0 +1,41 @@
+package com.cadastro.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "enderecos")
+public class Endereco {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, length = 9)
+    private String cep;
+
+    @Column(nullable = false, length = 100)
+    private String logradouro;
+
+    @Column(nullable = false, length = 10)
+    private String numero;
+
+    @Column(length = 50)
+    private String bairro;
+
+    @Column(length = 100)
+    private String cidade;
+
+    @Column(length = 20)
+    private String estado;
+
+    @OneToOne(mappedBy = "endereco")
+    private Cliente cliente;
+}
