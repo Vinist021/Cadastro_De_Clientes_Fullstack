111f41d633d793b2d98c12719dda32e2c4d696da
Vinicius Teixeira <vinist.002@gmail.com>
2025-08-16 13:44:21 -0300

    Closes #6 - Criada camada 'service'

---
+++ backend/backend-clientes/src/main/java/com/cadastro/service/ClienteService.java
@@ -0,0 +1,32 @@
+package com.cadastro.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.cadastro.model.Cliente;
+import com.cadastro.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+    
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente salvarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarClientes(){
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id){
+        return clienteRepository.findById(id);
+    }
+
+}
